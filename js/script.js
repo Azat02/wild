0ec9d2218f63d6f98bd40324275e9429c4332379
Azat  Ergeshbaev <nurtileknurbekov@Nurtileks-MacBook-Pro.local>
2023-02-27 16:28:20 +0600

    fifth

--- js/script.js
+++ js/script.js
@@ -5,28 +5,66 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const modalTrigger = document.querySelectorAll('.butter_buy'),
     modal = document.querySelector('.modal'),
-    closeModal = document.querySelector('[data-close]'),
-    close = document.querySelector('.btn_dark');
+    closeModal = document.querySelectorAll('[data-close]'),
+    close = document.querySelectorAll('.btn_dark');
     let modalInput = document.querySelectorAll('input');
+    let span = document.querySelector('.our_descr_span');
+    let discover = document.querySelector('.main .our_discover');
+    let sign = document.querySelector('.sign');
+    let signLink = document.querySelector('.sign_link');
+    let cookies = document.querySelector('.cookies');
+    let cookiesBtn = document.querySelector('.cookies_btn');
+    let cart = document.querySelector('.cart')
 
+    cookiesBtn.addEventListener('click', ()=>{
+        cookies.style.display = 'none';
+    })
+
+
+
+    signLink.addEventListener('click', function(){
+        sign.classList.remove('hide');
+        sign.classList.add('show')
+    })
+
+    discover.addEventListener('click' , () =>{
+        span.classList.remove('hide')
+        span.classList.add('show');
+    })
+
+    
 
     modalTrigger.forEach(elem => {
+        elem.addEventListener('click', incrementCount
+    );
+
+    let count = 0;
+
+    function incrementCount() {
+        count++;
+        let counterElement = document.querySelector(".cart");
+        counterElement.inner = `cart(${count})`;
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        console.log(count);
+        };
+
+    closeModal.forEach(elem=>{
         elem.addEventListener('click', function(){
-            modal.classList.add('show');
-            modal.classList.remove('hide');
+            modal.classList.remove('show');
+            modal.classList.add('hide');
+            sign.classList.add('hide');
+            sign.classList.remove('show');
         })
     })
 
-    closeModal.addEventListener('click', function(){
-        modal.classList.remove('show');
-        modal.classList.add('hide');
-    })
-
-    close.addEventListener('click', function(){
-        modal.classList.remove('show');
-        modal.classList.add('hide');
-        modalInput.forEach(elem => {
-            elem.value.innerHTML = ' ';
+    close.forEach( elem => {
+        elem.addEventListener('click', function(e){
+            modal.classList.remove('show');
+            modal.classList.add('hide');
+            sign.classList.add('hide');
+            sign.classList.remove('show');
+            e.preventDefault();
         })
     })
 
@@ -44,3 +82,5 @@ window.addEventListener('DOMContentLoaded', () => {
         })
     })
 })
+
+});
