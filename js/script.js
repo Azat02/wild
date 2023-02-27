9f871e7efc78847361e5d108bc7a1fd1ded6c7a4
Azat  Ergeshbaev <nurtileknurbekov@Nurtileks-MacBook-Pro.local>
2023-02-27 12:55:44 +0600

    fourth

--- js/script.js
+++ js/script.js
@@ -3,6 +3,35 @@ window.addEventListener('DOMContentLoaded', () => {
     menuItem = document.querySelectorAll('.menu_item'),
     hamburger = document.querySelector('.hamburger');
 
+    const modalTrigger = document.querySelectorAll('.butter_buy'),
+    modal = document.querySelector('.modal'),
+    closeModal = document.querySelector('[data-close]'),
+    close = document.querySelector('.btn_dark');
+    let modalInput = document.querySelectorAll('input');
+
+
+    modalTrigger.forEach(elem => {
+        elem.addEventListener('click', function(){
+            modal.classList.add('show');
+            modal.classList.remove('hide');
+        })
+    })
+
+    closeModal.addEventListener('click', function(){
+        modal.classList.remove('show');
+        modal.classList.add('hide');
+    })
+
+    close.addEventListener('click', function(){
+        modal.classList.remove('show');
+        modal.classList.add('hide');
+        modalInput.forEach(elem => {
+            elem.value.innerHTML = ' ';
+        })
+    })
+
+    
+
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('hamburger_active');
         menu.classList.toggle('menus_active');
